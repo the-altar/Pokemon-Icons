739bac3c2df75b70e7e7b0bab346cbfa42ceefa6
testuser <joaovictor.clementino@gmail.com>
2020-02-13 18:46:35 -0300

    make it better

--- main.go
+++ main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 
+	port := os.Getenv("PORT")
 	log.Println("Listening...")
-	err := http.ListenAndServe(":3000", http.FileServer(http.Dir("public")))
+
+	err := http.ListenAndServe(port, http.FileServer(http.Dir("public")))
 
 	if err != nil {
 		log.Printf("Error running web server for static assets: %v", err)
